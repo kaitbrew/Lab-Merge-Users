593d5deb270091d7638f44f8dd3a3cea29981002
kait brewer <kaitlynmbrewer@gmail.com>
2025-05-15 23:14:50 -0400

    tests worked this time! wahoo!

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require("datejs");
 
+function combineUsers(...args) {
+  //args will take the form of an array of array
+  let date = Date.parse("today");
+  let combinedObject = { users: [], merge_date: date.toString("M/d/yyyy") };
 
+  args.forEach((arg) => {
+    combinedObject.users.push(...arg);
+  });
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
